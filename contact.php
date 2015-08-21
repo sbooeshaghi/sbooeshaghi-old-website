95777982491890a25515ff359db9fc2a4ef4c95f
Sina Booeshaghi <sbooeshaghi@gmail.com>
2015-08-21 01:16:59 -0400

    contact form still not working

--- contact.php
+++ contact.php
@@ -1,69 +1,27 @@
-<?PHP
-/* Set e-mail recipient */
-    $myemail = "f245946f9cde4b95a5d1a38eb312a281.protect@whoisguard.com";
-
-    /* Check all form inputs using check_input function */
-    $name = check_input($_POST['inputName'], "Your Name");
-    $email = check_input($_POST['inputEmail'], "Your E-mail Address");
-    $subject = check_input($_POST['inputSubject'], "Message Subject");
-    $message = check_input($_POST['inputMessage'], "Your Message");
-
-    /* If e-mail is not valid show error message */
-    if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
-        {
-        show_error("Invalid e-mail address");
-        }
-    /* Let's prepare the message for the e-mail */
-
-    $email_subject = "[PWS] New Message";
-
-    $message = "
-
-    Someone has sent you a message using your contact form:
-
-    Name: $name
-    Email: $email
-    Subject: $subject
-
-    Message:
-    $message
-
-    ";
-
-    /* Send the message using mail() function */
-    mail($myemail, $email_subject, $message, $headers);
-
-    /* Redirect visitor to the thank you page */
-    header('Location: confirm.html');
-    //echo "Mail sent, thank you"
-    exit();
-
-    /* Functions we used */
-    function check_input($data, $problem='')
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        if ($problem && strlen($data) == 0)
-            {
-            show_error($problem);
-            }
-    return $data;
+<?php
+    $inputName = $_POST['inputName'];
+    $inputEmail = $_POST['inputEmail'];
+    $inputMessage = $_POST['inputMessage'];
+    $from = 'From: Peronal Website'; 
+    $to = 'contactsina@mit.edu'; 
+    $subject = '[PWS] New Message';
+    $inputHuman = $_POST['inputHuman'];
+			
+    $body = "From: $inputName\n E-Mail: $inputEmail\n Message:\n $inputMessage";
+				
+if ($_POST['submit']) {
+    if ($inputName != '' && $inputEmail != '') {
+        if ($inputHuman == '4') {				 
+            if (mail ($to, $subject, $body, $from)) { 
+	        echo '<p>Your message has been sent!</p>';
+	    } else { 
+	        echo '<p>Something went wrong, go back and try again!</p>'; 
+	    } 
+	} else if ($_POST['submit'] && $inputHuman != '4') {
+	    echo '<p>You answered the anti-spam question incorrectly!</p>';
+	}
+    } else {
+        echo '<p>You need to fill in all required fields!!</p>';
     }
-
-    function show_error($myError)
-    {
-        ?>
-        <html>
-        <body>
-
-        <p>Please correct the following error:</p>
-        <strong><?php echo $myError; ?></strong>
-        <p>Hit the back button and try again</p>
-
-        </body>
-        </html>
-        <?php
-        exit();
 }
 ?>
